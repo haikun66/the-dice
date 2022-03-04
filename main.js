59c2dab1775e51fcd2d3d1f876de0d8a842b60a0
haikun66 <85195787+haikun66@users.noreply.github.com>
2022-03-04 20:32:57 +0100

    new content
    new image, css, html and js

--- main.js
+++ main.js
@@ -1,40 +1,31 @@
 // Player name
 var player1 = "Player 1";
 var player2 = "Player 2";
+var playeractuel = "";
 
-// Function to change the player name
-function editNames() {
-    player1 = prompt("Change Player1 name");
-    player2 = prompt("Change player2 name");
+//fonction debut du jeux
+function gamestart(){
 
-    document.querySelector("p.Player1").innerHTML = player1;
-    document.querySelector("p.Player2").innerHTML = player2;
+if (nb_aleat(2) > 1) {
+    playeractuel = player1;
+    alert(player1 + " commence la partie.");
+  } 
+
+else {
+    playeractuel = player2;
+    alert(player2 + " commence la partie.");
+  }
 }
+function rolldice() {
+    
+        var randomNumber = Math.floor(Math.random() * 6) + 1;
 
-// Function to roll the dice
-function rollTheDice() {
-    setTimeout(function () {
-        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-        document.querySelector(".img1").setAttribute("src",
-            "dice" + randomNumber1 + ".png");
-
-        document.querySelector(".img2").setAttribute("src",
-            "dice" + randomNumber2 + ".png");
-
-        if (randomNumber1 === randomNumber2) {
-            document.querySelector("h1").innerHTML = "Draw!";
+        if (randomNumber > 1) {
+            alert("gagner");
         }
 
-        else if (randomNumber1 < randomNumber2) {
-            document.querySelector("h1").innerHTML
-                            = (player2 + " WINS!");
+        else if (randomNumber === 1) {
+            alert("perdu")
         }
 
-        else {
-            document.querySelector("h1").innerHTML
-                            = (player1 + " WINS!");
-        }
-    }, 2500);
-}
+    }
