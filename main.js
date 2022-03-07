1f5f45040beb38acff1686a5e70ed71c5896e638
haikun66 <85195787+haikun66@users.noreply.github.com>
2022-03-07 23:05:07 +0100

    file html and js modified
    file js modified same for index.html

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 // Player name
-var player1 = "Player 1";
-var player2 = "Player 2";
-var playeractuel = "";
+const player1 = "Player 1";
+const player2 = "Player 2";
+let playeractuel = "";
 
-//fonction debut du jeux
+let score1 = "";
+let score2 = "";
+let global1 = "";
+let global2 = "";
+let Dice = "";
+
+function entierAleatoire(min, max)
+{
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//La variable contient un nombre aléatoire compris entre 1 et 2
+let entier = entierAleatoire(1, 2);
+
 function gamestart(){
 
-if (nb_aleat(2) > 1) {
+if (entier > 1) {
     playeractuel = player1;
     alert(player1 + " commence la partie.");
   } 
@@ -15,17 +27,63 @@ else {
     playeractuel = player2;
     alert(player2 + " commence la partie.");
   }
+ 
+  do {
+    alert("au tour de " + playeractuel)
+    
+  } while (document.querySelector(".global1").innerHTML === 20|| document.querySelector(".global2").innerHTML === 20);
+  alert("winner " + playeractuel)
 }
+
+
 function rolldice() {
     
-        var randomNumber = Math.floor(Math.random() * 6) + 1;
+         Dice = Math.floor(Math.random() * 6) + 1;
 
-        if (randomNumber > 1) {
-            alert("gagner");
+        if (Dice > 1) {
+            if(playeractuel === player1){
+                score1 = Number(score1) + Number(Dice);
+                global1 = Number(score1) + Number(global1);
+                document.querySelector(".resulta1").innerHTML = score1;
+            }
+            else{
+                score2 = Number(score2) + Number(Dice);
+                global2 = Number(score2) + Number(global2);
+                document.querySelector(".resulta2").innerHTML = score2;
+            }
         }
 
-        else if (randomNumber === 1) {
+        else if(Dice === 1) {
             alert("perdu")
+            score1 = 0;
+            document.querySelector(".resulta1").innerHTML = score1;
+            score2 = 0;
+            document.querySelector(".resulta2").innerHTML = score2;
+            if (playeractuel === player1){
+                playeractuel = player2
+            }
+            else if(playeractuel === player2){
+                playeractuel = player1
+            }
         }
 
     }
+
+function hold() {
+    if (playeractuel === player1){
+        playeractuel = player2;
+        score1 = 0;
+        document.querySelector(".resulta1").innerHTML = score1;
+        score2 = 0;
+        document.querySelector(".resulta2").innerHTML = score2;
+        document.querySelector(".global1").innerHTML = global1;
+    }
+    else{
+        playeractuel = player1;
+        score1 = 0;
+        document.querySelector(".resulta1").innerHTML = score1;
+        score2 = 0;
+        document.querySelector(".resulta2").innerHTML = score2;
+        document.querySelector(".global2").innerHTML = global2;
+    }
+}
