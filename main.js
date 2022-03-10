a17103ca8b94f92eb7a548541536303890825ca4
haikun66 <85195787+haikun66@users.noreply.github.com>
2022-03-10 02:08:02 +0100

    correction de bug js and css add

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 // Player name
 const player1 = "Player 1";
 const player2 = "Player 2";
+const endgame =  document.getElementById('holld');
 let playeractuel = "";
-
 let score1 = "";
 let score2 = "";
 let global1 = "";
 let global2 = "";
 let Dice = "";
+let entier = entierAleatoire(1, 2);
 
 function entierAleatoire(min, max)
 {
  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-//La variable contient un nombre aléatoire compris entre 1 et 2
-let entier = entierAleatoire(1, 2);
 
 function gamestart(){
 
@@ -27,58 +26,61 @@ else {
     playeractuel = player2;
     alert(player2 + " commence la partie.");
   }
- 
-  do {
-    alert("au tour de " + playeractuel)
-    
-  } while (document.querySelector(".global1").innerHTML === 20|| document.querySelector(".global2").innerHTML === 20);
-  alert("winner " + playeractuel)
+
 }
 
-
 function rolldice() {
-    
-         Dice = Math.floor(Math.random() * 6) + 1;
-
+    Dice = Math.floor(Math.random() * 6) + 1;
+    let imgdice = document.getElementById("dee");
+    imgdice.src = "image/dice" + Dice + ".png";
         if (Dice > 1) {
             if(playeractuel === player1){
-                score1 = Number(score1) + Number(Dice);
-                global1 = Number(score1) + Number(global1);
-                document.querySelector(".resulta1").innerHTML = score1;
+                score1 = Number(score1) + Number(Dice); //score1 prend la valeur de dice.
+                document.querySelector(".resulta1").innerHTML = Number(score1); //score1 et afficher dans l'html
             }
-            else{
-                score2 = Number(score2) + Number(Dice);
-                global2 = Number(score2) + Number(global2);
-                document.querySelector(".resulta2").innerHTML = score2;
-            }
+                else{
+                    score2 = Number(score2) + Number(Dice);
+                    document.querySelector(".resulta2").innerHTML = Number(score2);
+                }
         }
+            else if(Dice === 1) {
+                score1 = 0;
+                document.querySelector(".resulta1").innerHTML = Number(score1);
+                score2 = 0;
+                document.querySelector(".resulta2").innerHTML = Number(score2);
 
-        else if(Dice === 1) {
-            alert("perdu")
-            score1 = 0;
-            document.querySelector(".resulta1").innerHTML = score1;
-            score2 = 0;
-            document.querySelector(".resulta2").innerHTML = score2;
-            if (playeractuel === player1){
-                playeractuel = player2
+                if (playeractuel === player1){
+                    playeractuel = player2
+                    document.getElementById('point2').style.display="block";
+                    document.getElementById('point1').style.display="none";
+                }
+                    else if(playeractuel === player2){
+                        playeractuel = player1
+                        document.getElementById('point1').style.display="block";
+                        document.getElementById('point2').style.display="none";
+                    }
             }
-            else if(playeractuel === player2){
-                playeractuel = player1
-            }
-        }
+        
 
     }
 
 function hold() {
     if (playeractuel === player1){
+        document.getElementById('point2').style.display="block";
+        document.getElementById('point1').style.display="none";
+        global1 = Number(global1) + Number(score1); //global1 prend la valeur de score1.
         playeractuel = player2;
         score1 = 0;
         document.querySelector(".resulta1").innerHTML = score1;
         score2 = 0;
         document.querySelector(".resulta2").innerHTML = score2;
         document.querySelector(".global1").innerHTML = global1;
+        
     }
     else{
+        document.getElementById('point1').style.display="block";
+        document.getElementById('point2').style.display="none";
+        global2 = Number(global2) + Number(score2); //global1 prend la valeur de score2.
         playeractuel = player1;
         score1 = 0;
         document.querySelector(".resulta1").innerHTML = score1;
@@ -87,3 +89,4 @@ function hold() {
         document.querySelector(".global2").innerHTML = global2;
     }
 }
+
